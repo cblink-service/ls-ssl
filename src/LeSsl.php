00e0ad7638af3384b92742c6270140f46ce1f200
Nick <i@httpd.cc>
2021-01-29 11:19:32 +0800

    fixbug

--- src/LeSsl.php
+++ src/LeSsl.php
@@ -11,6 +11,7 @@ use Cblink\Service\Kennel\ServiceContainer;
  * @property-read Auth\Client $auth         认证配置
  * @property-read RR\Client $rr             记录管理
  * @property-read Deploy\Client $deploy     部署记录
+ * @property-read Job\Client $job           队列
  */
 class LeSsl extends ServiceContainer
 {
